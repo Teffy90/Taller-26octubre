000143deb8a965a83faec72d23f9f89b70b3ad51
aledezma25 <aledezma@unal.edu.co>
2023-10-26 09:48:02 -0500

    Se crea inputs para formulario

--- front-movil/App.js
+++ front-movil/App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Formulario input con nombre, apellido, edad, cantidad de acompañantes */}
+      <Text style={styles.label}>Nombre</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+      />
+      <Text style={styles.label}>Apellido</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Apellido"
+      />
+      <Text style={styles.label}>Edad</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType='number-pad'
+        placeholder="Edad"
+      />
+      <Text style={styles.label}>Cantidad de acompañantes</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType='number-pad'
+        placeholder="Cantidad de acompañantes"
+      />
+      {/* Botón para enviar el formulario */}
+      <Button title="Enviar" />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +42,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
+    paddingLeft: 5,
   },
 });
