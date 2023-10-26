3fd35e86259b8c410bfc7795cc294b84f5d6d228
aledezma25 <aledezma@unal.edu.co>
2023-10-26 09:58:55 -0500

    Se realiza la conexion con axios

--- front-movil/App.js
+++ front-movil/App.js
@@ -1,36 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [edad, setEdad] = useState('');
+  const [acompanantes, setAcompanantes] = useState('');
+
+  const enviarDatosAlBackend = () => {
+    // Recolecta los datos de los TextInput
+    const data = {
+      nombre: nombre,
+      apellido: apellido,
+      edad: edad,
+      acompanantes: acompanantes,
+    };
+
+    // Envía los datos al servidor
+    axios.post('url del backend', data)
+      .then((response) => {
+        // Datos enviados con éxito	
+        console.log('Datos enviados con éxito');
+      })
+      .catch((error) => {
+        // Maneja errores si es necesario
+        console.error('Error al enviar datos', error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      {/* Formulario input con nombre, apellido, edad, cantidad de acompañantes */}
       <Text style={styles.label}>Nombre</Text>
       <TextInput
         style={styles.input}
         placeholder="Nombre"
+        value={nombre}
+        onChangeText={(text) => setNombre(text)}
       />
       <Text style={styles.label}>Apellido</Text>
       <TextInput
         style={styles.input}
         placeholder="Apellido"
+        value={apellido}
+        onChangeText={(text) => setApellido(text)}
       />
       <Text style={styles.label}>Edad</Text>
       <TextInput
         style={styles.input}
-        keyboardType='number-pad'
         placeholder="Edad"
+        keyboardType='number-pad'
+        value={edad}
+        onChangeText={(text) => setEdad(text)}
       />
       <Text style={styles.label}>Cantidad de acompañantes</Text>
       <TextInput
         style={styles.input}
-        keyboardType='number-pad'
         placeholder="Cantidad de acompañantes"
+        keyboardType='number-pad'
+        value={acompanantes}
+        onChangeText={(text) => setAcompanantes(text)}
       />
-      {/* Botón para enviar el formulario */}
-      <Button title="Enviar" />
-      
+      <Button title="Enviar" onPress={enviarDatosAlBackend} />
       <StatusBar style="auto" />
     </View>
   );
